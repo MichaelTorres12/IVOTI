fad2571ec3175cc024e43bc6bcd86e68fc103298
Torrescallejas <ctorrescallejas@gmail.com>
2023-11-24 22:25:59 -0600

    Se agrega los votos

--- src/main/java/com/project/ivoti/interfaceService/IvotoService.java
+++ src/main/java/com/project/ivoti/interfaceService/IvotoService.java
@@ -1,5 +1,8 @@
 package com.project.ivoti.interfaceService;
 
+import com.project.ivoti.Models.Candidato;
+import com.project.ivoti.Models.CentroVotacion;
+import com.project.ivoti.Models.Votante;
 import com.project.ivoti.Models.Voto;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +15,7 @@ public interface IvotoService {
     public List<Voto> listar();
 
     public Optional<Voto> buscarById(int id);
+
+    public void save(Candidato id_candidato, Votante dui, CentroVotacion id_centro_votacion);
 
 }
--- src/main/java/com/project/ivoti/service/VotoService.java
+++ src/main/java/com/project/ivoti/service/VotoService.java
@@ -1,11 +1,18 @@
 package com.project.ivoti.service;
 
+import com.project.ivoti.Models.Candidato;
+import com.project.ivoti.Models.CentroVotacion;
+import com.project.ivoti.Models.Votante;
 import com.project.ivoti.Models.Voto;
 import com.project.ivoti.interfaceService.IvotoService;
 import com.project.ivoti.interfaces.IVoto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +30,25 @@ public class VotoService implements IvotoService {
     @Override
     public Optional<Voto> buscarById(int id) {
         return voto.findById(id);
+    }
+
+
+
+    @Override
+    public void save(Candidato id_candidato, Votante dui, CentroVotacion id_centro_votacion) {
+
+        // Obtener la hora actual en formato LocalTime
+        LocalTime horaActual = LocalTime.now();
+        // Convertir LocalTime a Time
+        Time hora_voto = Time.valueOf(horaActual);
+
+
+        Voto voto1 = new Voto();
+        voto1.setHora_voto(hora_voto);
+        voto1.setDui(dui);
+        voto1.setId_candidato(id_candidato);
+        voto1.setId_centro_votacion(id_centro_votacion);
+
+        voto.save(voto1);
     }
 }
